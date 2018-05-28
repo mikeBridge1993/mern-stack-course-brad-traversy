bbb9fd7abff4ffa4ddf3efa5af0642d96b0ed41d
mikeBridge1993 <miguelponte93@hotmail.com>
2018-05-28 13:34:29 +0000

    User model and user registration implemented

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+
+const app = express();
  
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 //DB config
 const db = require("./config/keys").mongoURI;
 
@@ -13,15 +20,13 @@ mongoose.connect(db)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-const app = express();
-
 app.get("/", (req, res) => res.send("Hello"));
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
-//Use routes
+//Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
