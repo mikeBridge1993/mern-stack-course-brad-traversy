e07c8568c58468e9d4aeea3d2582b218feac46f2
mikeBridge1993 <miguelponte93@hotmail.com>
2018-07-11 11:32:59 +0100

    Get and delete posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,8 @@ const passport = require('passport');
 
 //Post model
 const Post = require('../../models/Post')
+//Profile model
+const Profile = require('../../models/Profile');
 
 //Validation
 const validatePostInput = require('../../validation/post');
@@ -15,10 +17,19 @@ const validatePostInput = require('../../validation/post');
 //@access Public
 router.get('/test', (req, res) => res.json({msg: "Posts route works"}));
 
-//@route  POST api/posts/test
+//@route  POST api/posts
 //@desc   Create post 
 //@access Private
 router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+
+  const {erros, isValid} = validatePostInput(req.body);
+
+  //Check validation
+  if(!isValid) {
+    //If any errors, send 400 with error object
+    return res.status(400).json(errors)
+  }
+
   const newPost = new Post ({
     text: req.body.text,
     name: req.body.name,
@@ -28,5 +39,48 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
 
   newPost.save().then(post => res.json(post));
 });
+
+//@route  GET api/posts
+//@desc   Get posts 
+//@access Public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({date: -1})
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({nopostsfound: "No posts found"}));
+});
+
+//@route  GET api/posts/:id
+//@desc   Get individual post by id 
+//@access Public
+router.get('/:id', (req, res) => {
+  Post.findById({id: req.params.id})
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({nopostfound: "No post found with that ID"}));
+});
+
+//@route  DELTE api/posts/:id
+//@desc   Delete individual post by id 
+//@access Private
+router.delete('/:id', passport.authenticate('jwt', {session: false }), (req, res) => {
+  Profile.findOne({user: req.user.id})
+    .then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          //Check for post owner
+          if(post.user.toString() !== req.user.id) {
+            return res.status(401).json({ notauthorized: 'User not authorized'})
+          }
+        })
+
+        //Delete
+        post.remove().then(() =>  res.json({ success: true }))
+      })
+});
+
+
+
+
+
 
 module.exports = router;
