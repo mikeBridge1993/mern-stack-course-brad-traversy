d8a0c8153aff4b630b349ea16dcfef33d43a5927
mikeBridge1993 <miguelponte93@hotmail.com>
2018-07-16 14:57:57 +0100

    Redux implementation, register and login completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken'
+import { setCurrentUser } from './actions/authActions'
+
+import store from './store'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Landing from './components/layout/Landing'
+
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+
 import './App.css';
+
+//Check for token in order to not lose it when page is reloading
+if(localStorage.jwtToken) {
+  //Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+
+  //Decode token and get user info and expiration
+  const decoded = jwt_decode(localStorage.jwtToken);
+
+  //Set user and is authenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar></Navbar>
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/login" component={Login} />
+            </div>
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+            </div>
+            <Footer></Footer>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+          <div className="container">
+            <Link to="/" className="navbar-brand">DevConnector</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+      
+            <div className="collapse navbar-collapse" id="mobile-nav">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <Link to="/profiles" className="nav-link"> Developers
+                  </Link>
+                </li>
+              </ul>
+      
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link to="/register" className="nav-link">Sign Up</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/login" className="nav-link">Login</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+}
